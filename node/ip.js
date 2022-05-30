1b86e3af7d49bae40cf4a2ae512ada84afed1be0
ipdatacloud <105967398+ipdatacloud@users.noreply.github.com>
2022-05-30 17:40:37 +0800

    Update ip.js

--- node/ip.js
+++ node/ip.js
@@ -19,10 +19,10 @@ var loadFile = function (filepath) {
 	endArr = [RecordSize];
 	addrArr = [RecordSize];
 	for (var i = 0; i < RecordSize; i++) {
-		var p = 2052 + (i * 8);
+		var p = 2052 + (i * 9);
 		endArr[i] = data.readUInt32LE(p);
-		var offset = data.readUIntLE(4 + p, 3);//3 bit 无符号整型
-		var length = data.readUInt8(7 + p);//1 bit 无符号整型      
+		var offset = data.readUInt32LE(4 + p);
+		var length = data.readUInt8(8 + p);//1 bit 无符号整型      
 		addrArr[i] = data.slice(offset, offset + length).toString('utf-8');
 	}
 
@@ -34,15 +34,19 @@ var Get = function (ip) {
 	var ipArray = ip.split('.'), ipInt = ipToInt(ip), pref = parseInt(ipArray[0]);
 	var low = prefStart[pref], high = prefEnd[pref];
 	var cur = low == high ? low : Search(low, high, ipInt);
+	if (cur==100000000){
+		return null;		
+	}
 	return addrArr[cur];
 }
 
 
 var Search = function (low, high, k) {
 	var M = 0;
+	var endipNum = 0
 	while (low <= high) {
 		var mid = Math.floor((low + high) / 2);
-		var endipNum = endArr[mid];
+		endipNum = endArr[mid];
 		if (endipNum >= k) {
 			M = mid;
 			if (mid === 0) {
@@ -53,7 +57,11 @@ var Search = function (low, high, k) {
 		else
 			low = mid + 1;
 	}
-	return M;
+	var startipNum = endipNum - 255
+	if (startipNum <= k && endipNum+255 >= k) {
+		return M
+	}
+	return 100000000;
 }
 
 var ipToInt = function (ip) { return new Buffer(ip.split('.')).readUInt32BE(0) }
