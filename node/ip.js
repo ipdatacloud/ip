7c24bb49f78626871d25667422c51b700d6a9ed0
ipdatacloud <105967398+ipdatacloud@users.noreply.github.com>
2022-05-30 17:15:30 +0800

    Create ip.js

---
+++ node/ip.js
@@ -0,0 +1,68 @@
+"use strict";
+var fs = require('fs');
+
+var data = null;
+var prefStart = [256];
+var prefEnd = [256];
+var endArr = [];
+var addrArr = [];
+
+var loadFile = function (filepath) {
+
+	data = fs.readFileSync(filepath);
+	var RecordSize = data.readUInt32LE(0);
+	for (var k = 0; k < 256; k++) {
+		var i = k * 8 + 4;
+		prefStart[k] = data.readUInt32LE(i);
+		prefEnd[k] = data.readUInt32LE(i + 4);
+	}
+	endArr = [RecordSize];
+	addrArr = [RecordSize];
+	for (var i = 0; i < RecordSize; i++) {
+		var p = 2052 + (i * 8);
+		endArr[i] = data.readUInt32LE(p);
+		var offset = data.readUIntLE(4 + p, 3);//3 bit 无符号整型
+		var length = data.readUInt8(7 + p);//1 bit 无符号整型      
+		addrArr[i] = data.slice(offset, offset + length).toString('utf-8');
+	}
+
+
+};
+
+
+var Get = function (ip) {
+	var ipArray = ip.split('.'), ipInt = ipToInt(ip), pref = parseInt(ipArray[0]);
+	var low = prefStart[pref], high = prefEnd[pref];
+	var cur = low == high ? low : Search(low, high, ipInt);
+	return addrArr[cur];
+}
+
+
+var Search = function (low, high, k) {
+	var M = 0;
+	while (low <= high) {
+		var mid = Math.floor((low + high) / 2);
+		var endipNum = endArr[mid];
+		if (endipNum >= k) {
+			M = mid;
+			if (mid === 0) {
+				break;   //防止溢出
+			}
+			high = mid - 1;
+		}
+		else
+			low = mid + 1;
+	}
+	return M;
+}
+
+var ipToInt = function (ip) { return new Buffer(ip.split('.')).readUInt32BE(0) }
+
+exports.loadFile = function (file) {
+	if (data === null) {
+		loadFile(file);
+	}
+}
+
+
+exports.get = Get;
